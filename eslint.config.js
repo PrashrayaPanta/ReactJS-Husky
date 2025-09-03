5c318f0158b80f20bb7f435c9a4de1f504d10535
Prashraya Panta <prashrayapanta33@gmail.com>
2025-09-04 00:58:37 +0545

    feat: prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
-  globalIgnores(["dist", "node_modules", "build"]),
+  globalIgnores(['dist', 'node_modules', 'build']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+      globals: globals.browser
+    }
   },
 
   {
     rules: {
-      "no-console": "error",
-      "no-undef": "warn",
-    },
-  },
-]);
+      'no-console': 'error',
+      'no-undef': 'warn'
+    }
+  }
+])
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <>
       <h1>Hello k xa</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
